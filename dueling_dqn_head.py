0303cb7dc48de47b53073a80d62449b0dfc022d2
Albert Edward Mc Manus <albiemc666@hotmail.co.za>
2025-08-12 19:17:55 +0200

    Create dueling_dqn_head.py

---
+++ dueling_dqn_head.py
@@ -0,0 +1,40 @@
+# caosb_world_model/modules/dueling_dqn_head.py
+
+import torch
+import torch.nn as nn
+from caosb_world_model.modules.noisy_layers import NoisyLinear
+
+class DuelingDQNHead(nn.Module):
+    """
+    A Dueling DQN head which takes features as input and outputs Q-values.
+    
+    It consists of a value stream and an advantage stream. The NoisyLinear
+    layers are used for efficient exploration.
+    """
+    def __init__(self, feature_dim, action_dim):
+        super().__init__()
+        
+        # Value stream: estimates the value of the state
+        self.value_stream = nn.Sequential(
+            NoisyLinear(feature_dim, 128),
+            nn.ReLU(),
+            NoisyLinear(128, 1)
+        )
+        
+        # Advantage stream: estimates the advantage of each action
+        self.advantage_stream = nn.Sequential(
+            NoisyLinear(feature_dim, 128),
+            nn.ReLU(),
+            NoisyLinear(128, action_dim)
+        )
+
+    def forward(self, features):
+        """
+        Computes the Q-values from the feature vector.
+        
+        The Q-value is calculated as V(s) + A(s, a) - mean(A(s, a)).
+        """
+        value = self.value_stream(features)
+        advantages = self.advantage_stream(features)
+        q_values = value + (advantages - advantages.mean(dim=-1, keepdim=True))
+        return q_values
